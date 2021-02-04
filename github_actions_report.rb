4902a7c3b0890dbe7a1f01729eaa19d1342eb9d4
Daiana_Mezdrea <daiana.mezdrea@yahoo.com>
2021-02-04 11:39:11 +0200

    Add agent and os in GhA report

--- github_actions_report.rb
+++ github_actions_report.rb
@@ -6,16 +6,12 @@ require 'json'
 require 'rest-client'
 require_relative 'octokit_utils'
 require_relative 'options'
-
 options = parse_options
-
 parsed = load_url(options)
-
 util = OctokitUtils.new(options[:oauth])
-
 result_hash = []
 headers = { Authorization: "token #{options[:oauth]}" }
-
+count = 0
 parsed.each do |_k, v|
   limit = util.client.rate_limit!
   puts "Getting data from Github API for #{v['github']}"
@@ -27,35 +23,56 @@ parsed.each do |_k, v|
   sleep 2 # Keep Github API happy
   url = "https://api.github.com/repos/#{v['github']}/actions/workflows"
   result = RestClient.get(url, headers)
-
   data = JSON.parse(result.body)
-
-  runs = {}
-
+  runs_array = []
   data['workflows'].each do |wf|
     url = "https://api.github.com/repos/#{v['github']}/actions/workflows/#{wf['id']}/runs"
+    puts url
+    next unless wf['name'] == 'nightly'
+
     runs_json = RestClient.get(url, headers)
-    runs[wf['name']] = JSON.parse(runs_json)
+    runs = JSON.parse(runs_json)
+
+    runs['workflow_runs'].each do |run|
+      export_results = []
+      jobs_json = JSON.parse(RestClient.get(run['jobs_url'], headers))
+      jobs_json['jobs'].each do |job|
+        next unless job['name'].include?('Acceptance')
+
+        matches = job['name'].match(/Acceptance \((?<os>[a-z0-9_.-]*), (?<agent>[a-z0-9_.-]*)\)/)
+        export_results << {
+          "os": matches[:os],
+          "agent": matches[:agent],
+          "result": job['conclusion']
+        }
+      end
+      runs_array << {
+        "run_number": run['run_number'],
+        "html_url": run['html_url'],
+        "head_branch": run['head_branch'],
+        "os_agent": export_results
+      }
+    end
+    result_hash << {
+      "url": "https://github.com/#{v['github']}",
+      "name": v['title'],
+      "runs": runs_array
+    }
   end
 
-  result_hash << {
-    "url": "https://github.com/#{v['github']}",
-    "name": v['title'],
-    "workflows": data['workflows'],
-    "runs": runs
-  }
 rescue StandardError => e
   puts "#{v['title']} - Error: #{e}"
   result_hash << {
     "url": "https://github.com/#{v['github']}",
     "name": v['title'],
     "workflows": 'Not available',
-    "runs": 'Not available'
+    "runs": 'Not available',
+    "agent": 'Not available',
+    "os": 'Not available'
   }
 end
 
 html = ERB.new(File.read('github_actions_report.html.erb')).result(binding)
-
 File.open('GithubActionsReport.html', 'wb') do |f|
   f.puts(html)
 end
